09a49c632bb2d6875ca7ebf7068eec419a9ff5b4
alew11 <ajlewis1113@gmail.com>
2023-05-05 19:45:08 -0400

    Initial commit

---
+++ src/test/java/com/andrew/lewis/sdk/andrewlewissdk/AndrewLewisSdkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrew.lewis.sdk.andrewlewissdk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndrewLewisSdkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
